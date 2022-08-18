4bca6f4db46317f42e416c59385664663c75cfa9
shihab <shihab.gsc10@gmail.com>
2022-08-18 22:14:15 +0600

    add global lang

---
+++ lang/en/global.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'email_verified' => 'Your email verified successfully.',
+    'invalid_username_password' => 'Invalid nickname or password.',
+    'invalid_auth_token' => 'Invalid Auth Token.',
+    'invalid_current_password' => 'Invalid current password.',
+    'login_success' => 'You have successfully logged in',
+    'need_more_permissions' => 'This action requires more permissions.',
+    'not_found' => 'Not found.',
+    'password_doesnot_match' => 'Password Doesn\'t match',
+    'referral' => 'You already have a referral code.',
+    'register_success' => 'You have successfully registered',
+    'unauthorized_request' => 'You\'re not authorized to do this request.',
+    'verification_link_sent' => 'Verification email sent successfully.',
+];
