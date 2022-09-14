c4e15179146523a67364fe850f83a0fbbbbd29d4
shihab <shihab.gsc10@gmail.com>
2022-09-15 00:03:45 +0600

    update global lang

--- lang/en/global.php
+++ lang/en/global.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
+    'success' => 'Success :)',
+    'email_verified' => 'Your email verified successfully.',
+    'invalid_current_password' => 'Invalid current password.',
     'invalid_username_password' => 'Invalid nickname or password.',
     'invalid_auth_token' => 'Invalid Auth Token.',
-    'unauthorized_request' => 'You\'re not authorized to do this request.',
     'need_more_permissions' => 'This action requires more permissions.',
-    'email_verified' => 'Your email verified successfully.',
-    'verification_link_sent' => 'Verification email sent to your email.',
     'not_found' => 'Not found.',
     'password_doesnot_match' => 'Password Doesnot match',
-    'invalid_current_password' => 'Invalid current password.',
+    'unauthorized_request' => 'You\'re not authorized to do this request.',
+    'verification_link_sent' => 'Verification email sent to your email.',
 ];
