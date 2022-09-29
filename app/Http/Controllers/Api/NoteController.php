9ca457d2800baa921334905034e5c295d3f6509c
shihab <shihab.gsc10@gmail.com>
2022-09-29 23:21:10 +0600

    notes

--- app/Http/Controllers/Api/NoteController.php
+++ app/Http/Controllers/Api/NoteController.php
@@ -2,23 +2,41 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Note;
+use App\Http\Resources\NoteResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Models\Note;
 
 class NoteController extends BaseApiController
 {
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function index(Request $request)
     {
-        return response()->json([
-            'data' => Note::paginate(20)
-        ]);
+        return response()->json(Note::paginate($request->paginate ?? 20));
     }
 
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function store(Request $request)
     {
         $note = Note::create($request->all());
-        return response()->json([
-            'data' => $note
-        ]);
+
+        return $this->sendResponse($note);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Note $note
+     * @return JsonResponse
+     */
+    public function show(Note $note)
+    {
+        return $this->sendResponse(new NoteResource($note));
     }
 }
