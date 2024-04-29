f21310aa03ae5c71c824611c9c24742208557ce3
shihab <shihab.gsc10@gmail.com>
2024-04-29 14:41:32 +0600

    added notes for contacts

--- vue/src/views/Contact/ContactView.vue
+++ vue/src/views/Contact/ContactView.vue
@@ -69,20 +69,30 @@
                     address</label>
             </div>
 
-            <div class="relative z-0 mb-6 w-full group">
-                <input id="nid" v-model="model.nid" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" name="nid"
-                       placeholder=" "
-                        type="text"/>
-                <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                       for="nid">Nid</label>
+            <div class="grid md:grid-cols-2 md:gap-6">
+                <div class="relative z-0 mb-6 group">
+                    <input id="nid" v-model="model.nid" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" name="nid"
+                           placeholder=" "
+                           type="text"/>
+                    <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                           for="nid">Nid</label>
+                </div>
+
+                <div class="relative z-0 mb-6 w-full group">
+                    <input id="dob" v-model="model.dob" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" name="nid"
+                           placeholder=" "
+                           type="date"/>
+                    <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                           for="">Date of Birth</label>
+                </div>
             </div>
 
-            <div class="relative z-0 mb-6 w-full group">
-                <input id="dob" v-model="model.dob" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" name="nid"
-                       placeholder=" "
-                        type="date"/>
+            <div class="relative z-0 mb-6 group">
+                <textarea id="" v-model="model.notes" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" name=""
+                       placeholder="write notes about this contact"
+                       />
                 <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                       for="">Date of Birth</label>
+                       for="nid">Notes</label>
             </div>
 
             <div class="relative z-0 mb-6 w-full group">
